a9e8117466393e730e2156eaf5e69fdca6f86b96
yejiLee <dpwlals1017@gmail.com>
2025-07-30 23:48:23 +0900

    🎉 Init: 프로젝트 초기 세팅 및 환경 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
           height={38}
           priority
         />
+        <h1>Hello</h1>
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
