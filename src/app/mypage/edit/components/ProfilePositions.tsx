0f134f3aa30a7534969fd377fb275e0e792601d2
yejiLee <dpwlals1017@gmail.com>
2025-08-21 01:57:04 +0900

    🔄 Refactor: 컴포넌트로 포지션 버튼 분리로 인한 코드개선

--- src/app/mypage/edit/components/ProfilePositions.tsx
+++ src/app/mypage/edit/components/ProfilePositions.tsx
@@ -1,3 +1,4 @@
+import PositionSmallButton from "@/components/common/PositionSmallButton";
 import { Button } from "@/components/ui/button";
 import { POSITION } from "@/constants/position";
 
@@ -9,17 +10,8 @@ const ProfilePositions = () => {
           <h2 className="h2-b">희망하는 포지션</h2>
           <span className="caption-r text-gray-50">최대 3개 선택</span>
         </div>
-        <div className="h-[92px] flex items-center gap-4 p-7 bg-white input-shadow">
-          {POSITION.map((position) => (
-            <Button
-              key={position}
-              variant="position"
-              size="custom"
-              className="w-[99px] h-9 cursor-pointer"
-            >
-              {position}
-            </Button>
-          ))}
+        <div>
+          <PositionSmallButton className="h-[92px] flex items-center gap-4 p-7 bg-white input-shadow" />
         </div>
       </section>
     </div>
