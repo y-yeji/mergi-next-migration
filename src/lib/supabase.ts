b1d29c1386b8e4e2ba647b8864b3586da7f95e97
yejiLee <dpwlals1017@gmail.com>
2026-05-11 20:36:11 +0900

    🔄 Refactor : Supabase 클라이언트 파일을 utils 폴더에서 lib 폴더로 이동

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,7 +2,7 @@ import { Database } from "@/database.types";
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
 
 // Supabase 클라이언트 인스턴스 생성
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
